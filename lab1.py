b6d971108ed406c767f25d2e8acce73bac4c12af
acdelgad <antondelgado95@gmail.com>
2018-04-10 10:44:40 -0700

    y'all owe me a beer

--- lab1.py
+++ lab1.py
@@ -2,14 +2,24 @@ import sys
 
 
 def read_file(filename):
-    print "implement me"
+    fp = open(filename, "r")
+    fp.readline()
+    toReturn = fp.read()
+    fp.close()
+    print(toReturn)
+    return toReturn
 
 
 def algorithm(filename):
-    print "implement me"
+    s = read_file(filename)
+    count = 0
+    for i in range(0, len(s)):
+       if s[i] == 'G' or s[i] == 'C':
+          count += 1
+    return (count / len(s)) * 100
 
 def print_results(filename):
-    print "implement me"
+    print(algorithm(filename))
 
 if __name__ == "__main__":
-    sequence = read_file(sys.argv[1])
+    print_results(sys.argv[1])
