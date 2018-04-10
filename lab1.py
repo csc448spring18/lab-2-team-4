5c06f78917c1b1197a3b1787fa23d6d40f5d34dd
c-hamilton <chamil03@calpoly.edu>
2018-04-10 10:17:28 -0700

    methods made

---
+++ lab1.py
@@ -0,0 +1,15 @@
+import sys
+
+
+def read_file(filename):
+    print "implement me"
+
+
+def algorithm(filename):
+    print "implement me"
+
+def print_results(filename):
+    print "implement me"
+
+if __name__ == "__main__":
+    sequence = read_file(sys.argv[1])
